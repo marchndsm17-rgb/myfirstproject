29f83032d8b7e46c61701615fd2e8b9d1c25c007
marchndsm17-rgb <marchndsm17@gmail.com>
2026-05-25 10:24:01 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-age = 18
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-if age>+ 18:
-     print("Adult:")
-else:
-   print("Minor")
+# Test it
+for num in range(1, 20):
+    if is_prime(num):
+        print(f"{num} is prime")
