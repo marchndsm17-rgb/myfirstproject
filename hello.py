0704334cf0ad197dfda3889a873558525242f1cb
marchndsm17-rgb <marchndsm17@gmail.com>
2026-05-25 10:41:33 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,4 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+name = "Marc"
 
-# Test it
-for num in range(1, 20):
-    if is_prime(num):
-        print(f"{num} is prime")
-        
+print("Hello, " + name)
+print("Welcome to Python!")
