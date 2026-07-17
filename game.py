55a1ba85e1610c3463aa92c932f063e77cd3ea92
marchndsm17-rgb <marchndsm17@gmail.com>
2026-07-17 10:02:44 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,6 @@
-print("Welcome to my game!")
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/USERNAME/REPOSITORY.git
+git push -u origin main
